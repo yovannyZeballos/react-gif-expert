60ff42b401eb37d4070832b0e30d5b01d9f43266
Yovanny Zeballos <yovanny.zeballos@gmail.com>
2022-06-22 13:08:25 -0500

    First commit

---
+++ src/components/GifItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const GifItem = ({url, title}) => {
+  return (
+    <div className="card">
+        <img src={url} alt={title} />
+        <p>{title}</p>
+
+    </div>
+  )
+}
